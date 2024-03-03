62845fc00b4b078e770711062de279e75a543ead
Samad-11 <samadmalik04@gmail.com>
2024-03-03 06:37:06 +0530

    add:animationText

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -2,24 +2,24 @@ import React from 'react'
 
 const HeroText = () => {
     return (
-        <div className="mb-10 flex justify-center items-center">
-            <div className="C">
+        <div className="mb-10 flex justify-center items-center ">
+            <div className="C heroText1">
                 <span className='sm:text-sm text-[0.5rem] faunaOne md:tracking-[1rem] font-black'>Jims</span>
                 <h1 className='lg:text-[15rem] md:text-[10rem] sm:text-9xl text-6xl tracking-widest font-extrabold uppercase cinzel' style={{ lineHeight: ".9" }}>C</h1>
             </div>
-            <div className="Y">
+            <div className="Y heroText2">
                 <span className='sm:text-sm text-[0.5rem] faunaOne md:tracking-[1rem] font-black'>Present</span>
                 <h1 className='lg:text-[15rem] md:text-[10rem] sm:text-9xl text-6xl tracking-widest font-extrabold uppercase cinzel' style={{ lineHeight: ".9" }}>Y</h1>
             </div>
-            <div className="N">
+            <div className="N heroText3">
                 <span className='sm:text-sm text-[0.5rem] faunaOne md:tracking-[1rem] font-black'>The</span>
                 <h1 className='lg:text-[15rem] md:text-[10rem] sm:text-9xl text-6xl tracking-widest font-extrabold uppercase cinzel' style={{ lineHeight: ".9" }}>N</h1>
             </div>
-            <div className="E">
+            <div className="E heroText4">
                 <span className='sm:text-sm text-[0.5rem] faunaOne md:tracking-[.7rem] font-black'>Greatest</span>
                 <h1 className='lg:text-[15rem] md:text-[10rem] sm:text-9xl text-6xl tracking-widest font-extrabold uppercase cinzel' style={{ lineHeight: ".9" }}>E</h1>
             </div>
-            <div className="T">
+            <div className="T heroText5">
                 <span className='sm:text-sm text-[0.5rem]  faunaOne md:tracking-[1rem] font-black break-words'>IT Fest</span>
                 <h1 className='lg:text-[15rem] md:text-[10rem] sm:text-9xl text-6xl tracking-widest font-extrabold uppercase cinzel' style={{ lineHeight: ".9" }}>T</h1>
             </div>
