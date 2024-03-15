cb2841969324ab7988f50baa24b4a724c384c1c3
Samad-11 <samadmalik04@gmail.com>
2024-03-15 05:40:53 +0530

    fixCountdown

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -6,27 +6,53 @@ import { log } from 'console';
 
 const Clock = () => {
     const dateNow = new Date();
-    console.log('====================================');
-    console.log(dateNow);
-
     const eventDate = new Date('March 15,2024, 6:30:00');
+    // Set the date we're counting down to
+    var countDownDate = new Date('March 15,2024, 6:30:00').getTime();
+    const [remainDays, setRemainDays] = useState<number>(eventDate.getDate() - dateNow.getDate() - 0)
+    //remain hours of the current day
+    const [remainHours, setRemainHours] = useState<number>(0);
+    // remain minutes in current hour
+    const [remainMinutes, setRemainMinutes] = useState<number>(0);
+    //remaining seconds in current minutes
+    const [remainSeconds, setRemainSeconds] = useState<number>(0);
+    const [isTimeNotLeft, setIsTimeLeft] = useState(false)
 
-    console.log('====================================');
-    console.log(eventDate.getDate() - dateNow.getDate(), " ", eventDate.getHours() - dateNow.getHours(), " ", eventDate.getMinutes() - dateNow.getMinutes());
-    console.log('====================================');
+    var x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        setRemainMinutes(minutes);
+        setRemainHours(hours)
+        setRemainSeconds(seconds)
+
+        // Display the result in the element with id="demo"
 
 
-    const [isTimeLeft, setIsTimeLeft] = useState(true)
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            setIsTimeLeft(true)
+        }
+    }, 1000);
+
+    // console.log('====================================');
+    // console.log(eventDate.getDate() - dateNow.getDate(), " ", eventDate.getHours() - dateNow.getHours(), " ", eventDate.getMinutes() - dateNow.getMinutes());
+    // console.log('====================================');
+
+
     //remain days
     // const remainDays = eventDate.getDate() - dateNow.getDate();
 
-    const [remainDays, setRemainDays] = useState<number>(eventDate.getDate() - dateNow.getDate() - 0)
-    //remain hours of the current day
-    const [remainHours, setRemainHours] = useState<number>(eventDate.getHours() - dateNow.getHours());
-    // remain minutes in current hour
-    const [remainMinutes, setRemainMinutes] = useState<number>(60 - dateNow.getMinutes());
-    //remaining seconds in current minutes
-    const [remainSeconds, setRemainSeconds] = useState<number>(60 - dateNow.getSeconds());
     const [seconds, setSeconds] = useState<number>(60);
 
     useEffect(() => {
@@ -36,24 +62,24 @@ const Clock = () => {
         //     remainSeconds >= 0)) {
 
 
-        const timer = setTimeout(() => {
-            setSeconds(prev => {
-                if (prev == 0) {
-                    return 60;
-                } else {
-                    return --prev
-                }
-            })
-            setRemainDays(eventDate.getDate() - dateNow.getDate() - 0)
-            setRemainHours(eventDate.getHours() - dateNow.getHours())
-            setRemainMinutes(60 - dateNow.getMinutes())
-            setRemainSeconds(60 - dateNow.getSeconds())
-            setIsTimeLeft(eventDate > dateNow)
-        }, 1000);
+        // const timer = setTimeout(() => {
+        //     setSeconds(prev => {
+        //         if (prev == 0) {
+        //             return 60;
+        //         } else {
+        //             return --prev
+        //         }
+        //     })
+        //     setRemainDays(eventDate.getDate() - dateNow.getDate() - 0)
+        //     setRemainHours(eventDate.getHours() - dateNow.getHours())
+        //     setRemainMinutes(60 - dateNow.getMinutes())
+        //     setRemainSeconds(60 - dateNow.getSeconds())
+        //     setIsTimeLeft(eventDate > dateNow)
+        // }, 1000);
 
-        return () => {
-            clearTimeout(timer);
-        };
+        // return () => {
+        //     clearTimeout(timer);
+        // };
         // }
     }, [dateNow, eventDate]);
     return (
@@ -61,9 +87,9 @@ const Clock = () => {
             <div className="flex flex-wrap gap-5 justify-center items-center mb-7">
                 <Countdown value={0} label='Days' />
                 {/* <Countdown value={isTimeLeft ? remainDays : 0} label='Days' /> */}
-                <Countdown value={isTimeLeft ? remainHours : 0} label='Hours' />
-                <Countdown value={isTimeLeft ? remainMinutes : 0} label='Minutes' />
-                <Countdown value={isTimeLeft ? remainSeconds : 0} label='Seconds' />
+                <Countdown value={isTimeNotLeft ? 0 : remainHours} label='Hours' />
+                <Countdown value={isTimeNotLeft ? 0 : remainMinutes} label='Minutes' />
+                <Countdown value={isTimeNotLeft ? 0 : remainSeconds} label='Seconds' />
             </div>
         </>
     )
--- src/components/HeroBounceText.tsx
+++ src/components/HeroBounceText.tsx
@@ -2,6 +2,24 @@ import Link from 'next/link'
 import React from 'react'
 
 const HeroBounceText = () => {
+    const dateNow = new Date();
+    const eventDate = new Date('March 15,2024, 5:30:00');
+
+    // if (dateNow < eventDate) {
+    //     return (
+    //         <Link href={'#events'} className='flex justify-center items-center'>
+    //             <h1 className='flex justify-center items-center gap-3 animate-bounce  w-fit'>
+    //                 <p className="mb-5 md:text-2xl text-sm font-semibold  faunaOne  tracking-wider">Registration Closed !
+    //                 </p>
+    //                 <p className='pb-4'>
+    //                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    //                         <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
+    //                     </svg>
+    //                 </p>
+    //             </h1>
+    //         </Link>
+    //     )
+    // }
     return (
         <Link href={'#events'} className='flex justify-center items-center'>
             <h1 className='flex justify-center items-center gap-3 animate-bounce  w-fit'>
